f2c22f030b36da4d76813f883a7458ffe1f47959
jpkneller <jpknelle@ncsu.edu>
2024-03-06 21:35:05 -0500

    Update input_class.EMEWS.cpp

--- src/input_class.EMEWS.cpp
+++ src/input_class.EMEWS.cpp
@@ -83,10 +83,11 @@ void Neutrino_loader(InputDataEMEWS ID,std::string &outputfilenamestem)
        theta13V = ID.theta13;
        theta23V = ID.theta23; 
 
-       etaV[0] = 0.;
-       etaV[1] = 0.;
-
        deltaV = ID.deltaCP;
+
+       etaV[0] = 0.;
+       etaV[1] = 0.;      
+       etaV[2] = -deltaV; // set this phase so that the top row of the PMNS matrix is pure real       
 
        // *********************
 
@@ -122,6 +123,10 @@ void Neutrino_loader(InputDataEMEWS ID,std::string &outputfilenamestem)
        theta23V*=M_PI/180.; c23V=cos(theta23V); s23V=sin(theta23V);
 
        deltaV*=M_PI/180.; cdeltaV=cos(deltaV); sdeltaV=sin(deltaV);
+
+       etaV[0]*=M_PI/180.;
+       etaV[1]*=M_PI/180.;
+       etaV[2]*=M_PI/180.;      
       }
 
        
