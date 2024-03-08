7cfcd77a68e250c14d2bd3b9436a6903d9d2c0ab
jpkneller <jpknelle@ncsu.edu>
2024-03-08 14:43:42 -0500

    Update mixing_angles.cpp

--- src/mixing_angles.cpp
+++ src/mixing_angles.cpp
@@ -28,10 +28,9 @@ MATRIX<complex<double>,NF,NF> MixingMatrix(MATRIX<complex<double>,NF,NF> Hf,arra
               //if(r2[e]>=r2[mu] && r2[e]>=r2[tau]){ u[e][j]=C<e,e>(Hf,k[j])/sqrt(r2[e]);       u[mu][j]=C<e,mu>(Hf,k[j])/sqrt(r2[e]);     u[tau][j]=C<e,tau>(Hf,k[j])/sqrt(r2[e]);}
               //if(r2[mu]>=r2[e] && r2[mu]>=r2[tau]){ u[e][j]=C<mu,e>(Hf,k[j])/sqrt(r2[mu]);    u[mu][j]=C<mu,mu>(Hf,k[j])/sqrt(r2[mu]);   u[tau][j]=C<mu,tau>(Hf,k[j])/sqrt(r2[mu]);}
               //if(r2[tau]>=r2[e] && r2[tau]>=r2[mu]){ u[e][j]=C<tau,e>(Hf,k[j])/sqrt(r2[tau]); u[mu][j]=C<tau,mu>(Hf,k[j])/sqrt(r2[tau]); u[tau][j]=C<tau,tau>(Hf,k[j])/sqrt(r2[tau]);}
+             
               // set the element in the e (top) row to be pure real
-              /*if(j==0 || j==1 || j==2)*/{ u[e][j]=C<e,e>(Hf,k[j])/sqrt(r2[e]); u[mu][j]=C<e,mu>(Hf,k[j])/sqrt(r2[e]); u[tau][j]=C<e,tau>(Hf,k[j])/sqrt(r2[e]);}
-              // set the element in the tau (bottom) row to be pure real for the 3rd column
-              //if(j==2){ u[e][j]=C<tau,e>(Hf,k[j])/sqrt(r2[tau]); u[mu][j]=C<tau,mu>(Hf,k[j])/sqrt(r2[tau]); u[tau][j]=C<tau,tau>(Hf,k[j])/sqrt(r2[tau]);}
+              u[e][j]=C<e,e>(Hf,k[j])/sqrt(r2[e]); u[mu][j]=C<e,mu>(Hf,k[j])/sqrt(r2[e]); u[tau][j]=C<e,tau>(Hf,k[j])/sqrt(r2[e]);
             }
        
         return u;
