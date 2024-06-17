1d3b8833c1cf2eec3011007f918762bf290dc79c
jpkneller <jpknelle@ncsu.edu>
2024-06-17 16:22:09 -0400

    Delete update.h

--- update.h
+++
@@ -1,9 +0,0 @@
-
-#include "EMEWS.h"
-
-#ifndef update_H
-#define update_H
-
-std::vector<std::vector<MATRIX<std::complex<double>,NF,NF> > > UpdateSm(double lambdaminus,double lambdaplus,std::vector<std::vector<std::array<double,NY> > > &Y,std::vector<std::vector<MATRIX<std::complex<double>,NF,NF> > > &Smprior);
-
-#endif
