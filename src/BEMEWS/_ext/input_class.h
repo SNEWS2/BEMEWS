7de0684b8682fb6a5fbf388556ef4897517bb768
jpkneller <jpknelle@ncsu.edu>
2024-07-01 15:00:55 -0400

    Update and rename input_class.h to input_class.h

--- src/BEMEWS/_ext/input_class.h
+++ src/BEMEWS/_ext/input_class.h
@@ -1,21 +1,21 @@
 
-#include "EMEWS.h"
+#include "BEMEWS.h"
 
 #ifndef input_class_H
 #define input_class_H
 
 // **************************************
 
-struct InputDataEMEWS;
+struct InputDataBEMEWS;
 
-void Profile_loader(InputDataEMEWS ID,std::string &outputfilenamestem);
-void Neutrino_loader(InputDataEMEWS ID,std::string &outputfilenamestem);
+void Profile_loader(InputDataBEMEWS ID,std::string &outputfilenamestem);
+void Neutrino_loader(InputDataBEMEWS ID,std::string &outputfilenamestem);
 
 // **********************************************************
 // **********************************************************
 // **********************************************************
 
-struct InputDataEMEWS 
+struct InputDataBEMEWS 
        { double altitude, azimuth; // in decimal degrees, altitude is negative for angles below the horizon
          std::string outputfilenamestem;
          std::string densityprofile;
@@ -26,10 +26,10 @@ struct InputDataEMEWS
          double deltam_21, deltam_32; // in eV^2	
          double theta12, theta13, theta23, deltaCP; // all in degrees
          double accuracy;
-         double stepcounterlimit; // how often it spits out data
-         bool outputflag; // whether the code outputs data as it does the integgration
+         double stepcounterlimit; // how often it outputs data
+         bool outputflag; // whether the code outputs data as it does the integration
 
-         InputDataEMEWS(void) {;}  
+         InputDataBEMEWS(void) {;}  
         };
 
 #endif
