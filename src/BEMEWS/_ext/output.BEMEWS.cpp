39432fe7771ca903a6a9757f911da8aa160abec2
jpkneller <jpknelle@ncsu.edu>
2025-10-07 14:32:00 -0400

    Update output.BEMEWS.cpp
    typo in UnitMatrix

--- src/BEMEWS/_ext/output.BEMEWS.cpp
+++ src/BEMEWS/_ext/output.BEMEWS.cpp
@@ -75,9 +75,9 @@ void Output_Pvslambda(bool firsttime,bool lasttime,ofstream &fPvslambda,double l
         int i;
 
         for(i=0;i<=imin-1;i++)
-	       { Sm[nu][i] = Sm[antinu][i] = UnitMatrix<complex<double> >(NF,NF);
-             Sfm[nu][i] = Sfm[antinu][i] = UnitMatrix<complex<double> >(NF,NF);	   
-             Sf[nu][i] = Sf[antinu][i] = UnitMatrix<complex<double> >(NF,NF);             
+	       { Sm[nu][i] = Sm[antinu][i] = UnitMatrix<complex<double> >(NF);
+             Sfm[nu][i] = Sfm[antinu][i] = UnitMatrix<complex<double> >(NF);	   
+             Sf[nu][i] = Sf[antinu][i] = UnitMatrix<complex<double> >(NF);             
             }       
        
         #pragma omp parallel for schedule(static)
